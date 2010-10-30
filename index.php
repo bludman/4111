bf49c7981f3e65e4b307a4bd4b1ef41519dcc458
bludman <bludman@gmail.com>
2010-10-30 00:49:09 -0400

    Problems connecting to db 

--- index.php
+++ index.php
@@ -1,11 +1,47 @@
+<html>
+<head>
+<title> Test </title>
+
+</head>
+<body>
+
+
+
 <?php
-print phpinfo();
+//print phpinfo();
+echo "hello world";
 ?>
 
 
 
 <?php
 require_once "connection.php";
-echo $db;
+echo "<br />". $db;
+
+
+
+ini_set('display_errors','On');
+$stid = oci_parse($conn, 'SELECT * FROM Sites');
+$err=oci_execute($stid);
+echo $err;
+
+echo "<table border='1'>\n";
+while ($row = oci_fetch_array($stid, OCI_ASSOC+OCI_RETURN_NULLS)) {
+    echo "<tr>\n";
+    foreach ($row as $item) {
+        echo "    <td>" . ($item !== null ? htmlentities($item, ENT_QUOTES) : "&nbsp;") . "</td>\n";
+    }
+    echo "</tr>\n";
+}
+echo "</table>\n";
+
+
+
+
+
+oci_close($conn);
 ?>
 
+
+</body>
+</html>
