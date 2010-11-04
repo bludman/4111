9026f651f24dc18ff034aa4128455b05c1ae2956
Elliot P Katz <epk2102@moscow.clic.cs.columbia.edu>
2010-11-04 18:39:59 -0400

    Added Search Form

---
+++ includes/pages/fragments/search_form.php
@@ -0,0 +1,9 @@
+<form name="input" id="search_form" action="index.php" method="get">
+<input type="hidden" name="page" value="search" />
+<input type="text" name="search_field">  
+<p><input type="checkbox" name="type" value="Buildings" />Building</p>
+<p><input type="checkbox" name="type" value="Eateries" /> Eatery</p>
+<p><input type="checkbox" name="type" value="Open_Spaces" />Open Space</p>
+<p><input type="checkbox" name="type" value="Monuments" /> Monument</p>
+<input type="submit" value="Search" />
+</form> 
