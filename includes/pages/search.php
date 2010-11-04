628a1caa4d3f520c47748bfb7773c38cd2a439c4
Elliot P Katz <epk2102@moscow.clic.cs.columbia.edu>
2010-11-04 18:39:59 -0400

    Added Search Form

--- includes/pages/search.php
+++ includes/pages/search.php
@@ -1,1 +1,20 @@
-<h2> THis is a search page </h2>
+<h2> This is a search page </h2>
+
+<?php
+  $conn= getConnection();
+
+  require("fragments/search_form.php");  
+
+  if ($_GET["search_field"]){
+    $searchInput = filter_var($_GET["search_field"], FILTER_VALIDATE_REGEXP, 
+      array("options"=>array("regexp"=>"/^[\w -]*$/")));
+      if (!$searchInput){
+        echo "<p>Bad Input Value</p>";
+      }
+    $stid = oci_parse($conn, 'SELECT id,name FROM Sites');
+    $err=oci_execute($stid);
+  }
+  else{
+  
+  }  
+?>
