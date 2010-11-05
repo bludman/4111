d2f34bd035d2bb17cfe1c4dd4f5a36aacc2a7c18
Elliot P Katz <epk2102@beirut.clic.cs.columbia.edu>
2010-11-04 23:24:33 -0400

    Login Created

---
+++ includes/pages/fragments/login_form.php
@@ -0,0 +1,5 @@
+<form action="index.php?page=login" method="post">
+  <p>Email Address <input type="text" name="email" /></p>
+  <p>Password <input type="password" name="password" /></p>
+  <input type="submit" value="Login" />
+</form>
