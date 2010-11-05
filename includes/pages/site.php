5b950ba2755dac735933ca3dd415660c44e17330
bludman <bludman@gmail.com>
2010-11-04 23:10:23 -0400

    Adding subtype detection to sites and some support for grabbing "more info"

--- includes/pages/site.php
+++ includes/pages/site.php
@@ -1,6 +1,9 @@
 <?php
 	include("includes/helpers/map_helper.php");
 	include("includes/helpers/menu_helper.php");
+	if(!is_numeric($_GET['id']))
+		die("shit, injection alert"); //TODO: 404
+
 	$conn= getConnection();
 	$stid = oci_parse($conn, "SELECT id,name,description,latitude,longitude FROM Sites S WHERE S.id=".$_GET['id']);
 	$err=oci_execute($stid);
@@ -26,7 +29,24 @@
 			case "image":
 					echo "image here";
 					break;
+			case "info":
+					$con= getConnection();
+					foreach(determineSubclass($_GET['id']) as $table)
+					{
+									echo "<p>table :$table";
+								$stid = oci_parse($con, "SELECT * FROM ".$table. " X WHERE X.site_id=".$_GET['id']);
+								$err=oci_execute($stid);
+								$row = oci_fetch_array($stid,OCI_BOTH+OCI_RETURN_NULLS);
+								foreach($row as $field)
+									echo "<p>$field";
+							
+
+					}
+					oci_close($con);
+					break;
 		}
+
+		
 		
 
 		
@@ -35,5 +55,34 @@
 
 
 	oci_close($conn);
+
+?>
+
+
+<?php
+
+/**
+ * Find which subtypes of Site a site belongs to
+ */
+function determineSubclass($siteId)
+{
+	$con= getConnection();
+	$tables= array("Buildings","Eateries","Open_Spaces","Monuments");
+	$foundOn = array();
+
+	foreach($tables as $table)
+	{
+		$stid = oci_parse($con, "SELECT S.id FROM Sites S, ".$table." X WHERE X.site_id= S.id AND S.id=".$_GET['id']);
+		$err=oci_execute($stid);
+		$row = oci_fetch_array($stid,OCI_BOTH+OCI_RETURN_NULLS);
+		if(!empty($row))
+			array_push($foundOn , $table);
+	}
+	
+	oci_close($con);
+
+	return $foundOn;
+
+}
 
 ?>
