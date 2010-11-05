6da488892e8d837f874e3e6fdbba69c5c31f2207
Elliot P Katz <epk2102@beirut.clic.cs.columbia.edu>
2010-11-04 20:59:15 -0400

    finished search

--- includes/pages/search.php
+++ includes/pages/search.php
@@ -10,15 +10,47 @@
       echo "<p class=\"error\">Bad Input Value</p>";
       require("fragments/search_form.php");
     }
-    for ($i = 1; $i <= 4; $i++){
-      if (isset($_GET['type'.$i])){
-        echo ($_GET['type'.$i]."<br>"); 
-      } 
+    else{
+      $select = " SELECT S.id, S.name";
+      $from = "FROM Sites S";
+      $where = "WHERE S.name LIKE '%" . $_GET["search_field"] ."%'";
+      $firstValue = false;
+      for ($i = 1; $i <= 4; $i++){
+        if (isset($_GET['type'.$i])){
+          //Set SELECT, FROM, WHERE
+           $from = $from . ", " . $_GET['type'.$i] . " X" . $i;
+           if (!$firstValue){
+                $where = $where . " AND (S.id=X" . $i . ".site_id";
+                $firstValue = true;
+           }
+           else{
+              $where = $where . " OR S.id=X" . $i . ".site_id"; 
+           }
+        } 
+      }
+      if ($firstValue){
+         $where = $where . ")";
+      }
+      $myQuery = $select . ' ' . $from . ' ' . $where;
+      echo($myQuery); 
+      $stid = oci_parse($conn, $myQuery);
+      $err=oci_execute($stid);
+      
+      echo "<table border='1'>\n";
+      while ($row = oci_fetch_array($stid,OCI_BOTH+OCI_RETURN_NULLS)) {
+        echo "<tr>\n";
+        echo "    <td>" .
+            "<a href=\"index.php?page=site&id=". $row['ID']."\">". 
+            ($row['NAME'] !== null ? htmlentities($row['NAME'], ENT_QUOTES) : "&nbsp;") . 
+            "</a></td>\n";
+      
+        echo "</tr>\n";
+      }
+      echo "</table>\n"; 
     }
-    $stid = oci_parse($conn, 'SELECT id,name FROM Sites');
-    $err=oci_execute($stid);
   }
-  else{
+  else {
     require("fragments/search_form.php");
   }
+  oci_close($conn);
 ?>
