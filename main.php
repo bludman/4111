4a6d4ab016770d60606d4b12753b3080ea646811
bludman <bludman@gmail.com>
2010-11-07 01:39:59 -0500

    fixing menu

--- main.php
+++ main.php
@@ -3,5 +3,6 @@
 	<li><a href="index.php?page=search">Search</a></li>
 	<li><a href="index.php?page=browse">Browse</a></li>
 	<li><a href="index.php?page=stats">Stats</a></li>
-	 <li><a href="index.php?page=history">Stats</a></li>
+  <li><a href="index.php?page=history">History</a></li>
+  <li><a href="index.php?page=scrap">Scrap Book</a></li>
 </ul>
