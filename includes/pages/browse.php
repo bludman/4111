f8c3a30dd43566f43a74c4297f59b1c332192549
bludman <bludman@gmail.com>
2010-11-08 22:05:29 -0500

    Refactoring site list display code to the site helper.

--- includes/pages/browse.php
+++ includes/pages/browse.php
@@ -1,10 +1,17 @@
 <h2> THis is a browse page </h2>
 
 <?php
+  include_once("includes/helpers/SiteHelper.php");
 	$conn= getConnection();
 	$stid = oci_parse($conn, 'SELECT id,name FROM Sites ORDER BY name');
 	$err=oci_execute($stid);
 
+
+  $nrows=oci_fetch_all($stid,$sites,0,-1,OCI_FETCHSTATEMENT_BY_ROW+OCI_ASSOC);
+  $siteHelper = new SiteHelper;
+  $siteHelper->showListOfSites($sites);
+  
+/*
 	echo "<table border='1'>\n";
 	while ($row = oci_fetch_array($stid,OCI_BOTH+OCI_RETURN_NULLS)) 
 	{
@@ -17,7 +24,7 @@
 		 echo "</tr>\n";
 	}
 	echo "</table>\n";
-
+*/
 
 
 
