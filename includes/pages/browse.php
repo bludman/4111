79fbebc683b8ee1552bbbeab1fa3ef9b2e945a4c
bludman <bludman@gmail.com>
2010-11-11 05:28:41 -0500

    More code cleanup

--- includes/pages/browse.php
+++ includes/pages/browse.php
@@ -3,7 +3,8 @@
 <?php
   include_once("includes/helpers/SiteHelper.php");
 	$conn= getConnection();
-	$stid = oci_parse($conn, 'SELECT id,name FROM Sites ORDER BY name');
+  $query="SELECT id,name FROM Sites ORDER BY name";
+	$stid = oci_parse($conn,$query);
 	$err=oci_execute($stid);
   $nrows=oci_fetch_all($stid,$sites,0,-1,OCI_FETCHSTATEMENT_BY_ROW+OCI_ASSOC);
   $siteHelper = new SiteHelper;
--- includes/pages/walkabout.php
+++ includes/pages/walkabout.php
@@ -4,50 +4,13 @@
   include_once("includes/helpers/SiteHelper.php");
   
 	$conn= getConnection();
-	$stid = oci_parse($conn, 'SELECT name FROM Walkabouts ORDER BY name');
-	$err=oci_execute($stid);
-  $nrows=oci_fetch_all($stid,$walkabouts,0,-1,OCI_FETCHSTATEMENT_BY_ROW+OCI_ASSOC);
-  $siteHelper = new SiteHelper;
+	$siteHelper = new SiteHelper;
   
   
   if(isset($_GET['walkabout']))
   {
-    //Showing a site in a walkabout
-    if(isset($_GET['n']) && is_numeric($_GET['n']))
-    {
-      $stid = oci_parse($conn, "SELECT S.id,S.name FROM comprised_of W, Sites S WHERE S.id= W.site_id AND W.walkabout_name='".$_GET['walkabout']."' ORDER BY name");
-      $err=oci_execute($stid);
-      $nrows=oci_fetch_all($stid,$sites,0,-1,OCI_FETCHSTATEMENT_BY_ROW+OCI_ASSOC);
-      
-      
-      $siteIndex=$_GET['n']; //index of this site in walkabout list 
-      $nextSiteIndex=$siteIndex+1;
-      $prevSiteIndex=$siteIndex-1;
-      
-      if(0<=$siteIndex && $siteIndex<count($sites))
-      { 
-        echo "<p>Site ".$siteIndex." is ".$sites[$siteIndex]['NAME']."</p>";
-        
-        if(0<=$nextSiteIndex && $nextSiteIndex<count($sites))  
-          echo "<p>Next site is ".($nextSiteIndex)." ".$sites[$nextSiteIndex]['NAME']."</p>";
-        
-        if(0<=$prevSiteIndex && $prevSiteIndex<count($sites))  
-          echo "<p>Prev site is ".$sites[$prevSiteIndex]['NAME']."</p>";
-       
-        
-       $_GET['id']=$sites[$siteIndex]['ID'];
-        include('site.php');
-       
-        
-      }
-      else
-      {
-        echo "<p>Invalid Site</p>";
-      }
-    }//listing a walkabout
-    else
-    {
       $auth= new Authenticator;
+          
           
       /* If user is logged in, collect data to see which of the sites in the walkabout he has visited*/
       if($auth->isLoggedIn())
@@ -68,10 +31,6 @@
         $query="SELECT S.id,S.name FROM comprised_of W, Sites S WHERE S.id= W.site_id AND W.walkabout_name='".$_GET['walkabout']."' ORDER BY name";
       }
         
-       
-        
-        
-        
       $stid = oci_parse($conn,$query);
       $err=oci_execute($stid);
       $nrows=oci_fetch_all($stid,$sites,0,-1,OCI_FETCHSTATEMENT_BY_ROW+OCI_ASSOC);
@@ -85,19 +44,19 @@
         echo "<p>No Matching Walkabout</p>";
       }
           
-    }
   }
   else
   {
+    $stid = oci_parse($conn, 'SELECT name FROM Walkabouts ORDER BY name');
+    $err=oci_execute($stid);
+    $nrows=oci_fetch_all($stid,$walkabouts,0,-1,OCI_FETCHSTATEMENT_BY_ROW+OCI_ASSOC);
+
+      
     echo "<ul class=\"menu\">";
     foreach($walkabouts as $walkabout)
     {
       
       echo "<li><a href=\"index.php?page=walkabout&walkabout=". $walkabout['NAME']."\">".$walkabout['NAME']."</li>";
-      // $stid = oci_parse($conn, "SELECT S.id,S.name FROM comprised_of W, Sites S WHERE S.id= W.site_id AND W.walkabout_name='".$walkabout['NAME']."' ORDER BY name");
-      // $err=oci_execute($stid);
-      // $nrows=oci_fetch_all($stid,$sites,0,-1,OCI_FETCHSTATEMENT_BY_ROW+OCI_ASSOC);
-      // $siteHelper->showListOfSites($sites);
     }
     echo "</ul>";
   }
