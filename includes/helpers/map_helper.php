7fc62bc5556b399b55f284ba691078d758c72a4d
bludman <bludman@gmail.com>
2010-11-11 07:27:58 -0500

    Bigger map, and zoomed in.

--- includes/helpers/map_helper.php
+++ includes/helpers/map_helper.php
@@ -3,9 +3,7 @@
 function outputMapImage($lat,$long)
 {
 ?>
-	<div>
-	<img width="300" height="100" style="border:1px solid #888888;" src="http://maps.google.com/maps/api/staticmap?center=<?php echo $lat.",".$long?>&amp;size=300x100&amp;maptype=roadmap&amp;markers=color:red|<?php echo $lat.",".$long?>&amp;zoom=15&amp;sensor=false&amp;" />
-	</div>
+	<img width="300" height="300" style="border:1px solid #888888;" src="http://maps.google.com/maps/api/staticmap?center=<?php echo $lat.",".$long?>&amp;size=300x300&amp;maptype=roadmap&amp;markers=color:red|<?php echo $lat.",".$long?>&amp;zoom=17&amp;sensor=false&amp;" />
 <?php
 }
 
